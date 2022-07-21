e35badd8c5730ba5c0d882def56c95c6660f91f3
Cleudeir <cleudeirsilva@gmail.com>
2022-07-21 11:30:36 -0300

    start

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,7 +2,7 @@
 
 export default async function data(req, res) {
 
-  const url = "http://192.168.1.10:3333/data"
+  const url = "https://mflixbackredecanais.herokuapp.com/data"
   const pull = await fetch(url)
   const data = await pull.json()
   res.status(200).json(data)
